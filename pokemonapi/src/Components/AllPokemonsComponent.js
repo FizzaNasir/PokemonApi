3f3af9252b070c1d63e66f87f0cb5ac32e2c48af
FizzaNasir <fizzanasir976@gmail.com>
2024-06-17 18:07:04 +0500

    Added components

---
+++ pokemonapi/src/Components/AllPokemonsComponent.js
@@ -0,0 +1,32 @@
+import { useGetAllPokemonsQuery } from '../Features/apiSlice';
+import { btnStyle } from '../styles'; 
+import { Link } from 'react-router-dom';
+
+const AllPokemons = () => {
+  const { data, isLoading, isError, error } = useGetAllPokemonsQuery()   
+
+  if (isLoading) {
+        return <div>Loading...</div>;
+  }
+  
+  if (isError) {
+      return <div>Error: {error.message}</div>;
+  }
+  return (
+
+    <ul>
+        {       
+          data.results.map((pokemon, index) => (
+            <li>
+                <div>Hello World</div>
+                <Link to={`/pokemon/${index + 1}`} >
+                  <button style={btnStyle}> {pokemon.name} </button>
+                </Link>
+            </li>
+          ))
+        }
+    </ul>
+  );
+}
+
+export default AllPokemons
